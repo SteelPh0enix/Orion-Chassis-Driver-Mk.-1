d8a6cb77014247a428e3eac3cf5aa8176b1d4944
Wojciech Olech <steelphoenix2137@gmail.com>
2019-01-04 20:42:10 +0100

    minor stuff

--- lib/Chassis/chassis.cpp
+++ lib/Chassis/chassis.cpp
@@ -9,8 +9,23 @@ Chassis::Chassis() {
   m_right_front_wheel.set_pins(Pinout::PWM_2, Pinout::DIR_2A, Pinout::DIR_2B,
                                Pinout::TEMPERATURE_2, Pinout::CURRENT_2);
   m_left_rear_wheel.set_pins(Pinout::PWM_3, Pinout::DIR_3A, Pinout::DIR_3B,
-                               Pinout::TEMPERATURE_3, Pinout::CURRENT_3);
+                             Pinout::TEMPERATURE_3, Pinout::CURRENT_3);
   m_right_rear_wheel.set_pins(Pinout::PWM_4, Pinout::DIR_4A, Pinout::DIR_4B,
-                               Pinout::TEMPERATURE_4, Pinout::CURRENT_4);
+                              Pinout::TEMPERATURE_4, Pinout::CURRENT_4);
 }
+
+void Chassis::initialize() {
+  m_left_front_wheel.initialize();
+  m_right_front_wheel.initialize();
+  m_left_rear_wheel.initialize();
+  m_right_rear_wheel.initialize();
+}
+
+void Chassis::set_speed(int16_t speed) {}
+
+int16_t Chassis::speed() const { return m_speed; }
+
+void Chassis::set_rotation(int16_t value) {}
+
+int16_t Chassis::rotation() const { return m_rotation; }
 }  // namespace Orion
--- lib/Chassis/chassis.hpp
+++ lib/Chassis/chassis.hpp
@@ -6,22 +6,27 @@
 
 namespace Orion {
 class Chassis {
-  public:
-    Chassis();
-    void initialize();
+ public:
+  Chassis();
+  void initialize();
 
-    // set_speed allows to set forward/backward speed
-    // Range: -255 : 255
-    void set_speed(int16_t speed);
+  // set_speed allows to set forward/backward speed
+  // Range: -255 : 255
+  void set_speed(int16_t speed);
+  int16_t speed() const;
 
-    // set_rotation allows to set left/right rotation of chassis
-    // Range: -255 : 255
-    void set_rotation(int16_t value);
+  // set_rotation allows to set left/right rotation of chassis
+  // Range: -255 : 255
+  void set_rotation(int16_t value);
+  int16_t rotation() const;
 
-  private:
-    Wheel m_left_front_wheel;
-    Wheel m_right_front_wheel;
-    Wheel m_left_rear_wheel;
-    Wheel m_right_rear_wheel;
+ private:
+  Wheel m_left_front_wheel;
+  Wheel m_right_front_wheel;
+  Wheel m_left_rear_wheel;
+  Wheel m_right_rear_wheel;
+
+  int16_t m_speed{};
+  int16_t m_rotation{};
 };
 }  // namespace Orion
